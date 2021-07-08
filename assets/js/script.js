d70ec069eeb57521b6df43438391e4058b2ba54e
Md Kawsar <kawsar721g@gmail.com>
2021-07-09 00:45:58 +0600

    responsiv concept

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,24 @@ navbarbutton.addEventListener('click', function () {
     navbarToggleShow.classList.toggle('show');
     navbarToggleShow.classList.toggle('collapsing');
 });
+
+// const navbar = document.querySelector('#navbar');
+// var dist = navbar.offsetTop;
+// window.scroll(function () {
+    
+//     var scrooling = this.scrollTop();
+//     console.dir(scrooling);
+// })
+$(function () {
+    var dist = $('.navbar').offset().top;
+    console.log($('.navbar').offset());
+    // alert(dist)
+    $(window).scroll(function () {
+        var scrooling = $(this).scrollTop();
+        if (scrooling >= dist) {
+            $('.navbar').addClass('stikey');
+        } else {
+            $('.navbar').removeClass('stikey');
+        }
+    })
+})
