127509f1302bae09745f43d64da6d2e19b03a56c
Md Kawsar <kawsar721g@gmail.com>
2021-07-09 11:39:07 +0600

    Trushed Cliend done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,72 @@ navbarbutton.addEventListener('click', function () {
     navbarToggleShow.classList.toggle('show');
     navbarToggleShow.classList.toggle('collapsing');
 });
+$('.client__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    Infinity: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+
+    // the magic
+    responsive: [
+        {
+
+        breakpoint: 1900,
+        settings: {
+            slidesToShow: 6,
+            infinite: true
+        }
+
+        },
+        {
+
+        breakpoint: 1201,
+        settings: {
+            slidesToShow: 6,
+            infinite: true
+        }
+
+        },
+        {
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 6,
+            infinite: true
+        }
+
+        },
+        {
+
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 4,
+            infinite: true
+        }
+
+        },
+        {
+
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 3,
+            dots: true
+        }
+
+        },
+        {
+
+        breakpoint: 320,
+        settings: {
+            slidesToShow: 2,
+            dots: true
+        }
+
+        },
+        ]
+});
 
 // const navbar = document.querySelector('#navbar');
 // var dist = navbar.offsetTop;
@@ -23,5 +89,7 @@ $(function () {
         } else {
             $('.navbar').removeClass('stikey');
         }
-    })
+    });
+  
+
 })
